18e4a78f722754c3afbc014cb512327f691ba044
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2023-09-16 09:25:48 +0530

    Add files via upload

---
+++ wrk6.py
@@ -0,0 +1,15 @@
+age=int(input("enter your age:"))
+if(age<18):
+    print("Not eligible for voting")
+elif(age>18):
+    print("eligible for voting")   
+
+k=2
+while k<=20:
+    print(k)    
+    k=k+2
+
+b=1
+while b<=19:
+    print(b)
+    b=b+2
