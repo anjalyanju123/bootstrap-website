3f0f68bba6f0c6f003dedc9fc54c98891e377eb6
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2023-09-19 09:12:36 +0530

    Add files via upload

--- wrk6.py
+++ wrk6.py
@@ -1,30 +1,30 @@
-# age=int(input("enter your age:"))
-# if(age<18):
-#     print("Not eligible for voting")
-# elif(age>18):
-#     print("eligible for voting")   
+age=int(input("enter your age:"))
+if(age<18):
+    print("Not eligible for voting")
+elif(age>18):
+    print("eligible for voting")   
 
-# k=2
-# while k<=20:
-#     print(k)    
-#     k=k+2
+k=2
+while k<=20:
+    print(k)    
+    k=k+2
 
-# b=1
-# while b<=19:
-#     print(b)
-#     b=b+2
-# s=int(input("enter the integer:"))
-# sqr=(s*s)
-# if(s<10):
-#     print(sqr)
-# else:
-#     print("invalid")    
+b=1
+while b<=19:
+    print(b)
+    b=b+2
+
+s=int(input("enter the integer:"))
+sqr=(s*s)
+if(s<10):
+    print(sqr)
+else:
+    print("invalid")    
       
                  
 num=int(input("enter the number:"))
-i=3
 if(num >1):
-       if(num%2==0 and num%i==0):
+       if(num%2==0):
               print("it is not prime")   
          
        else:
