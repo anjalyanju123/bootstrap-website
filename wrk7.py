b793ac7e938441d0a68ed66ceb10733a73ad64d8
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2023-09-20 09:08:19 +0530

    Add files via upload

--- wrk7.py
+++ wrk7.py
@@ -27,28 +27,49 @@
 #     list2.append(l)
 #     print(list2)
 # print("length is :",len(list2)) 
-l1=[]
+# l1=[]
+# n1=int(input("enter the size of list:"))
+# i=0
+# while i<n1:
+#     print("enter the element{}:".format(i+1))
+#     l=int(input())
+#     l1.append(l)
+#     i+=1
+
+# l2=[]
+# j=0
+# while j<n1:
+#     print("enter the element{}:".format(j+1))
+#     l=int(input())
+#     l2.append(l)
+#     j+=1
+# k=0
+# l3=[]
+# while k<n1:
+#     if l1[k] in l2:
+#         l3.append(l1[k])
+#     k+=1
+# print(l1)
+# print(l2)
+# print(l3)
+
+# strg=str(input("enter the string:"))
+# reverse=''
+# count=len(strg)
+# while count>0:
+#     reverse+=strg[count-1]
+#     count=count-1
+# print("reverse of",strg ,"is")
+# print(reverse)
+
+l4=[]
 n1=int(input("enter the size of list:"))
 i=0
 while i<n1:
     print("enter the element{}:".format(i+1))
     l=int(input())
-    l1.append(l)
+    l4.append(l)
     i+=1
-
-l2=[]
-j=0
-while j<n1:
-    print("enter the element{}:".format(j+1))
-    l=int(input())
-    l2.append(l)
-    j+=1
-k=0
-l3=[]
-while k<n1:
-    if l1[k] in l2:
-        l3.append(l1[k])
-    k+=1
-print(l1)
-print(l2)
-print(l3)
+print(l4)
+l4.sort()
+list=len(l4)
