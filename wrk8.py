ee717894b9854f6a077103713efad5f3380f7e59
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2023-09-27 09:10:49 +0530

    Add files via upload

--- wrk8.py
+++ wrk8.py
@@ -1,4 +1,25 @@
-b="python"
-for i in range():    
-     for j in range(8):
-       print(b[:j-1])    
+# b="python"
+# x = ""
+# n=5
+# for i in b:
+#     x += i
+#     print(x)
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(b[:], end="")
+#     print()
+# for i in range(n-1):
+#         for j in range(n - i - 1):
+#          print(b[:j-i],end="")
+#         print()
+
+string= input("Enter string :")   
+revstr="" 
+for i in string: 
+  revstr = i + revstr
+# print("Reversed string : ",revstr) 
+ 
+if(string==revstr): 
+   print("The string is a palindrome.") 
+else: 
+   print("The string is not a palindrome.") 
