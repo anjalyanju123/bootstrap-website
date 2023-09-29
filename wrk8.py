6654e0b65e8d0eeaa10f980e7713860db7bd7117
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2023-09-29 09:40:26 +0530

    Add files via upload

--- wrk8.py
+++ wrk8.py
@@ -1,25 +1,45 @@
 # b="python"
 # x = ""
-# n=5
-# for i in b:
-#     x += i
-#     print(x)
-# for i in range(n):
-#     for j in range(i + 1):
-#         print(b[:], end="")
-#     print()
-# for i in range(n-1):
-#         for j in range(n - i - 1):
-#          print(b[:j-i],end="")
-#         print()
+# n=int(input("enter the number of rows:"))
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=i:
+#       print("*",end=" ")
+#       j+=1
+#     print()  
+#     i+=1
+# using for loop
+# c=int(input("enter the number of rows:"))
+# for i in range(c):
+#     print("*"*(i+1))
 
-string= input("Enter string :")   
-revstr="" 
-for i in string: 
-  revstr = i + revstr
-# print("Reversed string : ",revstr) 
- 
-if(string==revstr): 
-   print("The string is a palindrome.") 
-else: 
-   print("The string is not a palindrome.") 
+# def add(a,b):
+#     sum=a+b
+#     print(sum)
+# add(6,9) 
+
+# a=int(input("enter first digit:"))
+# b=int(input("enter second digit:"))
+# def add(a,b):
+#     sum=a+b
+#     return sum
+# print("sum is :",add(a,b))
+b="python"
+n=len(b) 
+i=0
+while i<=n:
+    j=0
+    while j<=i-1:
+      print(b[j],end=" ")
+      j+=1      
+    i+=1
+    print()
+i=0    
+while i<=n:
+  k=0
+  while k<=n-i-1:
+    print(b[k],end=" ")
+    k+=1  
+  i+=1
+  print()     
