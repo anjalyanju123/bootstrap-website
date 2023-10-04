a6ae379441b8386741029d36357054c6c6bbe2ad
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2023-10-04 09:22:35 +0530

    Add files via upload

--- wrk9.py
+++ wrk9.py
@@ -15,13 +15,66 @@
 
 # character()
 # program to print sum of square of numbers
-def square():
-   num=input("enter the numbers:")
-   sq=0
-   for i in num:
-     print(i)
-     sq+=(int(i)**2)
-     print(sq)  
+# def square():
+#    num=input("enter the numbers:")
+#    sq=0
+#    for i in num:
+#      print(i)
+#      sq+=(int(i)**2)
+#      print(sq)  
 
-square()  
-      
+# square() 
+
+# program to print common elements in two lists 
+# list1=[]
+# n=int(input("enter the size of first list:"))
+# i=0
+# while i<n:
+#     print("enter the element{}:".format(i+1))
+#     l=int(input())
+#     i+=1
+#     list1.append(l)
+# list2=[]
+# i=0
+# while i<n:
+#     print("enter the element{}:".format(i+1))
+#     l=int(input())
+#     i+=1
+#     list2.append(l)
+# list3=[]
+# k=0
+# while k<n:
+#     if list1[k] in list2:
+#         list3.append(list1[k])
+#     k+=1    
+# print(list1)
+# print(list2)
+# print("common elements are :",list3)         
+#program to convert a decimal number to binary,octal and hexadecimal
+
+#decimal=input("enter the decimal number:")
+
+#program to find factorial of a number
+
+# num=int(input("enter the number:"))
+# def fact():
+#     if num==0:
+#         print("factorial of 0 is 1")
+#     elif(num<0):
+#         print("factorial of negative number is not posible")
+#     else:    
+#        result=1
+#        for i in range(1,num+1):
+#         result=result*i
+#        print("factorial is :",result)
+# fact()    
+# program to print febonacci series upto a limit
+num=int(input("enter the range:"))
+result=0
+a=0
+def sum(result):
+    for i in range(0,num):
+        a=result+i-1
+        result=a
+        print(result,end=",")
+sum(result)
