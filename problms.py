59dff671913220d20d784552ecaf18987df62333
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2023-10-10 09:17:52 +0530

    Add files via upload

--- problms.py
+++ problms.py
@@ -1,8 +1,16 @@
-num=[2,4,5]
-n=len(num)
-target=6
-for i in range(n):
-    for j in range(0,n):
-        if (num[i]+num[j]==target):
-            print([i,j]) 
-            break
+# num=[2,4,5]
+# n=len(num)
+# target=6
+# for i in range(n):
+#     for j in range(0,n):
+#         if (num[i]+num[j]==target):
+#             print([i,j]) 
+#             break
+
+#program to find the squareroot of a number
+num=int(input("enter a number:"))
+import  math
+x=math.sqrt(num)
+print("square root is:",x)
+
+
